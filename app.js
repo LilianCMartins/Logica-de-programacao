bf618566c0fcaba18971a3c8ada1b741bde0e6d8
Lilian Martins <lilian_martins3@hotmail.com>
2025-02-06 15:02:07 -0300

    Exercicios iniciais JS

---
+++ app.js
@@ -0,0 +1,8 @@
+alert ('Boas vindas ao nosso site!'); //Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+let nome = 'Lua'; // Crie uma variável nome
+let idade = 25; // Crie uma variável chamada idade e atribua a ela o valor 25.
+let numeroDevendas = 50; //Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let saldoDisponivel = 1000;//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+alert('Erro! Preencha todos os campos!') //Exiba um alerta com o texto "Erro! Preencha todos os campos"
+
